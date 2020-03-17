4e50e4814b003fd4e36ee4757540619e802d1120
Philip Kwesiga <phil.kwesiga@gmail.com>
2020-03-17 22:41:41 +0300

    remove typo

--- recipes/python.rb
+++ recipes/python.rb
@@ -2,6 +2,11 @@ execute "install-python" do
     command 'sudo add-apt-repository ppa:deadsnakes/ppa'
     command 'sudo apt-get update'
     command 'sudo apt-get install python3.6'
-    command 'sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1'
-    not_if { ::File.exists?(pip_binary) }
-  end
+end
+
+alternatives 'python-set-version-3' do
+    link_name 'python3'
+    path '/usr/bin/python3.6'
+    priority 100
+    action :install
+end
